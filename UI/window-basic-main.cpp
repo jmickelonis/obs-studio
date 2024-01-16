c8a3ea9c9068c13e0c2841ef0d7a16f2ee6f64c8
John Mickelonis <jmick@email.com>
2024-01-16 18:49:46 -0500

    Changed the application window title. Only show Profile/Scene names in the title if they're non-default.

--- UI/window-basic-main.cpp
+++ UI/window-basic-main.cpp
@@ -9442,18 +9442,25 @@ void OBSBasic::UpdateTitleBar()
 	const char *sceneCollection = config_get_string(
 		App()->GlobalConfig(), "Basic", "SceneCollection");
 
-	name << "OBS ";
-	if (previewProgramMode)
-		name << "Studio ";
+	// name << "OBS ";
+	// if (previewProgramMode)
+	// 	name << "Studio ";
+	name << "OBS Studio (jmick's Mod)";
 
-	name << App()->GetVersionString(false);
+	// name << App()->GetVersionString(false);
+	// if (App()->IsPortableMode())
+	// 	name << " - " << Str("TitleBar.PortableMode");
+
+	// Only show Profile/Scenes names if they're non-default
+	const char *untitled = Str("Untitled");
+	if (strcmp(profile, untitled))
+		name << " • " << Str("TitleBar.Profile") << ": " << profile;
+	if (strcmp(sceneCollection, untitled))
+		name << " • " << Str("TitleBar.Scenes") << ": "
+		     << sceneCollection;
+
 	if (safe_mode)
-		name << " (" << Str("TitleBar.SafeMode") << ")";
-	if (App()->IsPortableMode())
-		name << " - " << Str("TitleBar.PortableMode");
-
-	name << " - " << Str("TitleBar.Profile") << ": " << profile;
-	name << " - " << Str("TitleBar.Scenes") << ": " << sceneCollection;
+		name << " • " << Str("TitleBar.SafeMode") << ")";
 
 	setWindowTitle(QT_UTF8(name.str().c_str()));
 }
