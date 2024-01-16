143813b642d90c03ebd4aea820de7f6bbd030bdb
John Mickelonis <jmick@email.com>
2024-01-16 18:21:58 -0500

    Dim disabled icons app-wide (not just in the context area). Use a simpler and fully-opaque style for rubber bands. Made widget animations a little faster.

--- UI/obs-proxy-style.hpp
+++ UI/obs-proxy-style.hpp
@@ -1,16 +1,16 @@
 #pragma once
 
 #include <QProxyStyle>
+#include <QPainter>
 
 class OBSProxyStyle : public QProxyStyle {
 public:
+	void drawControl(ControlElement element, const QStyleOption *option,
+			 QPainter *painter,
+			 const QWidget *widget) const override;
+	QPixmap generatedIconPixmap(QIcon::Mode iconMode, const QPixmap &pixmap,
+				    const QStyleOption *option) const override;
 	int styleHint(StyleHint hint, const QStyleOption *option,
 		      const QWidget *widget,
 		      QStyleHintReturn *returnData) const override;
-};
-
-class OBSContextBarProxyStyle : public OBSProxyStyle {
-public:
-	QPixmap generatedIconPixmap(QIcon::Mode iconMode, const QPixmap &pixmap,
-				    const QStyleOption *option) const override;
 };
