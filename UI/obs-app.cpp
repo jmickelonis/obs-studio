03c5187ec6a13a55c9ad9c07a69dc21b6e399b61
John Mickelonis <jmick@email.com>
2024-10-10 03:41:28 -0400

    Activate AMDGPU Pro Vulkan and AMF automatically by default.

--- UI/obs-app.cpp
+++ UI/obs-app.cpp
@@ -83,6 +83,11 @@
 
 using namespace std;
 
+#include <QVersionNumber>
+#include <regex>
+#include <json11.hpp>
+using namespace json11;
+
 static log_handler_t def_log_handler;
 
 static string currentLogFile;
@@ -1896,6 +1901,104 @@ static bool UseWindowsDarkMode()
 }
 #endif
 
+#if !defined(_WIN32) && !defined(__APPLE__)
+static QFileInfo GetAMDGPUProICDPath()
+{
+	// Prefer standard ICD search paths.
+	QString homePath = QDir::homePath();
+	QStringList paths = {
+		homePath + "/.config",      "/etc/xdg",         "/usr/local/etc", "/etc",
+		homePath + "/.local/share", "/usr/local/share", "/usr/share",
+	};
+
+	for (QString s : paths) {
+		QFileInfo path(s + "/vulkan/icd.d/amd_pro_icd64.json");
+		if (path.exists() && path.isFile())
+			return path;
+	}
+
+	// Default to the one used by a standard proprietary driver installation.
+	return QFileInfo("/opt/amdgpu-pro/etc/vulkan/icd.d/amd_icd64.json");
+}
+
+static void ActivateAMF()
+{
+	const char *value = getenv("OBS_ACTIVATE_AMF");
+	if (!(value ? QVariant(value).toBool() : true)) {
+		blog(LOG_INFO, "AMD HW encoder activation was disabled (OBS_ACTIVATE_AMF=false)");
+		return;
+	}
+
+	// Check for the encoder library
+	std::unique_ptr<FILE, decltype(&pclose)> pipe(popen("ldconfig -v 2>/dev/null | grep libamfrt64", "r"), pclose);
+
+	if (!pipe)
+		return;
+
+	std::string config;
+	std::array<char, 64> buffer;
+	while (fgets(buffer.data(), static_cast<int>(buffer.size()), pipe.get()) != nullptr)
+		config += buffer.data();
+
+	std::regex pattern(R"(libamfrt64\.so\.(\d+\.\d+\.\d+))");
+	std::smatch match;
+	if (!std::regex_search(config, match, pattern))
+		return;
+
+	QString amfVersion = match[1].str().c_str();
+	blog(LOG_INFO, "Found AMD HW encoder: libamfrt64.so v%s", amfVersion.toStdString().c_str());
+
+	if (QVersionNumber::fromString(amfVersion) >= QVersionNumber(1, 4, 34) &&
+	    !(value && !strcmp(value, "AMDVLK-PRO"))) {
+		// AMF 1.4.34 introduced stable support for Mesa/RADV
+		blog(LOG_INFO,
+		     "Not using AMDVLK-PRO with AMF >= 1.4.34 by default (set OBS_ACTIVATE_AMF=AMDVLK-PRO to force)");
+		return;
+	}
+
+	QFileInfo file_info = GetAMDGPUProICDPath();
+	if (!(file_info.exists() && file_info.isFile()))
+		return;
+
+	QString path = file_info.filePath();
+	QFile file(path);
+	if (!file.open(QFile::ReadOnly | QFile::Text))
+		return;
+
+	QTextStream in(&file);
+	std::string error;
+	Json json = Json::parse(in.readAll().toStdString(), error);
+	if (!error.empty())
+		return;
+
+	Json icd = json["ICD"];
+	if (!icd.is_object())
+		return;
+
+	Json version = icd["api_version"];
+	Json library_path = icd["library_path"];
+	if (!(version.is_string() && library_path.is_string()))
+		return;
+
+	QDir dir(QFileInfo(library_path.string_value().c_str()).path());
+	blog(LOG_INFO,
+	     "Found AMDGPU Pro Vulkan ICD"
+	     "\n      Path: %s"
+	     "\n      Version: %s"
+	     "\n      Library Path: %s",
+	     path.toStdString().c_str(), version.string_value().c_str(), dir.path().toStdString().c_str());
+
+	QStringList fileNames(path);
+	const char *s = getenv("VK_ICD_FILENAMES");
+	if (s)
+		fileNames << QString(s).split(':', Qt::SkipEmptyParts);
+
+	qputenv("AMD_VULKAN_ICD", "AMDVLK-PRO");
+	qputenv("VK_ICD_FILENAMES", fileNames.join(':').toUtf8());
+	blog(LOG_INFO, "Using AMDVLK-PRO with AMF");
+}
+#endif
+
 static const char *run_program_init = "run_program_init";
 static int run_program(fstream &logFile, int argc, char *argv[])
 {
@@ -1949,6 +2052,8 @@ static int run_program(fstream &logFile, int argc, char *argv[])
 		}
 	}
 #endif
+
+	ActivateAMF();
 
 	QFileInfo vulkan_layer_d("/opt/share/vulkan/implicit_layer.d");
 	if (vulkan_layer_d.exists() && vulkan_layer_d.isDir()) {
