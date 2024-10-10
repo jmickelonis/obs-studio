20a71ed53c8a8e304b19df65bddf8ef599d85ed3
John Mickelonis <jmick@email.com>
2024-10-10 02:37:24 -0400

    Use a simpler and fully-opaque style for rubber bands. Dim disabled icons app-wide (not just in the context area). Made widget animations a little faster.

--- UI/obs-proxy-style.cpp
+++ UI/obs-proxy-style.cpp
@@ -1,5 +1,6 @@
 #include "obs-proxy-style.hpp"
 #include <QStyleOptionButton>
+#include <QPainterPath>
 
 static inline uint qt_intensity(uint r, uint g, uint b)
 {
@@ -7,13 +8,48 @@ static inline uint qt_intensity(uint r, uint g, uint b)
 	return (77 * r + 150 * g + 28 * b) / 255;
 }
 
+void OBSProxyStyle::drawControl(ControlElement element, const QStyleOption *option, QPainter *painter,
+				const QWidget *widget) const
+{
+	if (element == QStyle::CE_RubberBand) {
+		// Use a simpler and fully-opaque style for rubber bands
+		QRectF rect(option->rect);
+
+		static qreal borderThickness = 1;
+		static qreal halfBorderThickness = borderThickness / 2;
+		rect.adjust(halfBorderThickness, halfBorderThickness, -halfBorderThickness, -halfBorderThickness);
+
+		QPainterPath path;
+		path.addRoundedRect(rect, 2, 2);
+
+		const QPalette *palette = &option->palette;
+		QColor highlightColor = palette->color(QPalette::Highlight);
+		QColor windowColor = palette->color(QPalette::Window);
+
+		static float hcAmount = .25;
+		static float wcAmount = 1 - hcAmount;
+
+		QColor fillColor = QColor(windowColor.red() * wcAmount + highlightColor.red() * hcAmount,
+					  windowColor.green() * wcAmount + highlightColor.green() * hcAmount,
+					  windowColor.blue() * wcAmount + highlightColor.blue() * hcAmount);
+
+		painter->setRenderHint(QPainter::Antialiasing);
+		painter->setPen(QPen(highlightColor, borderThickness));
+		painter->fillPath(path, fillColor);
+		painter->drawPath(path);
+		return;
+	}
+
+	QProxyStyle::drawControl(element, option, painter, widget);
+}
+
 /* The constants in the default QT styles don't dim the icons enough in
  * disabled mode
  *
  * https://code.woboq.org/qt5/qtbase/src/widgets/styles/qcommonstyle.cpp.html#6429
  */
-QPixmap OBSContextBarProxyStyle::generatedIconPixmap(QIcon::Mode iconMode, const QPixmap &pixmap,
-						     const QStyleOption *option) const
+QPixmap OBSProxyStyle::generatedIconPixmap(QIcon::Mode iconMode, const QPixmap &pixmap,
+					   const QStyleOption *option) const
 {
 	if (iconMode == QIcon::Disabled) {
 		QImage im = pixmap.toImage().convertToFormat(QImage::Format_ARGB32);
@@ -79,6 +115,10 @@ int OBSProxyStyle::styleHint(StyleHint hint, const QStyleOption *option, const Q
 	if (hint == SH_ComboBox_UseNativePopup)
 		return 1;
 #endif
+
+	// Make widget animations a little faster
+	if (hint == SH_Widget_Animation_Duration)
+		return 150;
 
 	return QProxyStyle::styleHint(hint, option, widget, returnData);
 }
--- UI/obs-proxy-style.hpp
+++ UI/obs-proxy-style.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <QProxyStyle>
+#include <QPainter>
 
 class OBSProxyStyle : public QProxyStyle {
 public:
@@ -8,12 +9,10 @@ public:
 
 	OBSProxyStyle(const QString &key) : QProxyStyle(key) {}
 
-	int styleHint(StyleHint hint, const QStyleOption *option, const QWidget *widget,
-		      QStyleHintReturn *returnData) const override;
-};
-
-class OBSContextBarProxyStyle : public OBSProxyStyle {
-public:
+	void drawControl(ControlElement element, const QStyleOption *option, QPainter *painter,
+			 const QWidget *widget) const override;
 	QPixmap generatedIconPixmap(QIcon::Mode iconMode, const QPixmap &pixmap,
 				    const QStyleOption *option) const override;
+	int styleHint(StyleHint hint, const QStyleOption *option, const QWidget *widget,
+		      QStyleHintReturn *returnData) const override;
 };
