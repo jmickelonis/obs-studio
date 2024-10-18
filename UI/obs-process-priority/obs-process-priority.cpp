2de28c34dcb1e6207f59703e512785f2724952c9
John Mickelonis <jmick@email.com>
2024-10-18 15:25:25 -0400

    Cleaned up and simplified code. Added missing pclose call. Added the ability to set OBS's process priorities without knowing the PIDs.

--- UI/obs-process-priority/obs-process-priority.cpp
+++ UI/obs-process-priority/obs-process-priority.cpp
@@ -9,94 +9,167 @@
 #include <cstdlib>
 #include <filesystem>
 #include <iostream>
+#include <list>
 #include <sstream>
 #include <stdexcept>
 #include <stdio.h>
 #include <sys/resource.h>
 
-namespace fs = std::filesystem;
+using namespace std;
+using namespace std::filesystem;
 
-
-static bool parse_int(char* c, int& out) {
-    std::string s = c;
-    try {
-        std::size_t pos;
-        int i = std::stoi(s, &pos);
-        if (pos < s.size()) {
-            std::cerr << "Invalid int: " << s << std::endl;
-        }
-        else {
-            out = i;
-            return true;
-        }
-    } catch (std::invalid_argument const &ex) {
-        std::cerr << "Invalid int: " << s << std::endl;
-    } catch (std::out_of_range const &ex) {
-        std::cerr << "Int out of range: " << s << std::endl;
-    }
-    return false;
-}
-
+static string KDESU = "/opt/kde/lib/x86_64-linux-gnu/libexec/kf6/kdesu";
 static int ERROR = -1;
 
-int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        std::cerr << "Usage: obs-process-priority <pid> <priority>" << std::endl;
-        return ERROR;
-    }
+static bool parse_int(const char *c, int &i)
+{
+	string s = c;
+	try {
+		size_t pos;
+		i = stoi(s, &pos);
+		if (pos == s.size())
+			return true;
+		cerr << "Invalid int: " << s << endl;
+	} catch (invalid_argument const &ex) {
+		cerr << "Invalid int: " << s << endl;
+	} catch (out_of_range const &ex) {
+		cerr << "Int out of range: " << s << endl;
+	}
+	return false;
+}
 
-    int pid;
-    if (!parse_int(argv[1], pid))
-        return ERROR;
+static string run(const string cmd, int &res)
+{
+	FILE *pipe = popen(cmd.c_str(), "r");
 
-    int priority;
-    if (!parse_int(argv[2], priority))
-        return ERROR;
+	if (!pipe) {
+		cerr << "Command failed: " << cmd << endl;
+		res = ERROR;
+		return "";
+	}
 
-    std::ostringstream ss;
-    ss << "readlink -f /proc/" << pid << "/exe";
-    std::string cmd = ss.str();
+	ostringstream ss;
+	char buffer[128];
+	while (fgets(buffer, sizeof buffer, pipe) != NULL)
+		ss << buffer;
+	res = pclose(pipe);
 
-    char buffer[128];
-    FILE* pipe = popen(cmd.c_str(), "r");
+	string s = ss.str();
+	s.erase(remove(s.begin(), s.end(), '\n'), s.cend());
+	return s;
+}
 
-    if (!pipe) {
-        std::cerr << "Command failed: " << cmd << std::endl;
-        return ERROR;
-    }
+static void set_single_priority(const int id, const int priority, const bool try_direct = true)
+{
+	cout << "Setting priority " << priority << " for PID: " << id << endl;
+	if (try_direct) {
+		int res = setpriority(PRIO_PROCESS, id, priority);
+		if (!res)
+			return;
+	}
+	// Use kdesu to request elevation and use renice
+	ostringstream ss;
+	ss << KDESU << " -- renice " << priority << " " << id;
+	system(ss.str().c_str());
+}
 
-    ss.str("");
-    ss.clear();
-    while (fgets(buffer, sizeof buffer, pipe) != NULL)
-        ss << buffer;
+static void set_multiple_priority(const string exe, const int priority)
+{
+	// Get all process IDs using pidof
+	ostringstream ss;
+	ss << "pidof \"" << exe << "\"";
+	int res;
+	string output = run(ss.str(), res);
+	if (res) {
+		cerr << "No processes found for " << exe << endl;
+		return;
+	}
 
-    std::string process_path = ss.str();
-    process_path.erase(std::remove(process_path.begin(), process_path.end(), '\n'), process_path.cend());
+	// Parse the response into a list of ints
+	list<int> ids;
+	istringstream iss(output);
+	string id_str;
+	while (getline(iss, id_str, ' '))
+		ids.push_back(stoi(id_str));
 
-    if (process_path == "") {
-        std::cerr << "Could not find binary path for PID " << pid << std::endl;
-        return ERROR;
-    }
+	if (ids.size() == 1) {
+		// There's only 1 ID
+		set_single_priority(ids.front(), priority);
+		return;
+	}
 
-    fs::path directory = fs::canonical(argv[0]).parent_path();
-    fs::path process_directory = fs::canonical(process_path).parent_path();
+	cout << "Setting priority " << priority << " for IDs:";
+	for (int id : ids)
+		cout << " " << id;
+	cout << endl;
 
-    if (process_directory != directory) {
-        std::cerr << "Process does not appear to belong to OBS: " << process_path << std::endl;
-        return ERROR;
-    }
+	// Keep track of the IDs that couldn't be changed directly
+	list<int> error_ids;
+	for (int id : ids) {
+		int res = setpriority(PRIO_PROCESS, id, priority);
+		if (res)
+			error_ids.push_back(id);
+	}
 
-    std::cout << "Setting priority for PID " << pid << " to " << priority << std::endl;
+	if (error_ids.empty())
+		return;
 
-    int res = setpriority(PRIO_PROCESS, pid, priority);
-    if (res) {
-        // Attempt failed
-		// Use kdesu to request elevation and use renice
-        ss.str("");
-        ss.clear();
-		ss << "/opt/kde/lib/x86_64-linux-gnu/libexec/kf6/kdesu -- renice -n " << priority << " -p " << pid;
-		std::string s = ss.str();
-		res = system(s.c_str());
-    }
-    return res;
+	// Use kdesu to change the remaining IDs
+	if (error_ids.size() == 1) {
+		set_single_priority(error_ids.front(), priority, false);
+		return;
+	}
+
+	ss.str("");
+	ss.clear();
+	ss << KDESU << " -- bash -c 'for id in";
+	for (int id : error_ids)
+		ss << " " << id;
+	ss << "; do renice " << priority << " $id; done'";
+	system(ss.str().c_str());
+}
+
+int main(const int argc, const char *argv[])
+{
+	if (argc < 2 || argc > 3) {
+		cerr << "Usage: obs-process-priority <priority> [id]" << endl;
+		return ERROR;
+	}
+
+	int priority;
+	if (!parse_int(argv[1], priority))
+		return ERROR;
+
+	path directory = canonical(argv[0]).parent_path();
+
+	if (argc == 2) {
+		ostringstream ss;
+		ss << "\"" << directory.string() << "/obs\"";
+		set_multiple_priority(ss.str(), priority);
+		return 0;
+	}
+
+	int id;
+	if (!parse_int(argv[2], id))
+		return ERROR;
+
+	ostringstream ss;
+	ss << "readlink -f /proc/" << id << "/exe";
+	int res;
+	string process_path = run(ss.str(), res);
+
+	if (res) {
+		cerr << "Could not find binary path for PID " << id << endl;
+		return res;
+	}
+
+	path process_directory = canonical(process_path).parent_path();
+
+	if (process_directory != directory) {
+		cerr << "Process does not appear to belong to OBS: " << process_path << endl;
+		return ERROR;
+	}
+
+	set_single_priority(id, priority);
+	return 0;
 }
