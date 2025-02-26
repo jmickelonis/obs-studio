1b9be028a67a93c5bb188118c85eb56899d46586
John Mickelonis <jmick@email.com>
2025-02-26 17:37:48 -0500

    Enhanced broadcasting is not implemented on non-Windows.

--- UI/window-basic-settings.cpp
+++ UI/window-basic-settings.cpp
@@ -5622,9 +5622,9 @@ void OBSBasicSettings::UpdateMultitrackVideo()
 			ui->enableMultitrackVideo->setChecked(false);
 	}
 
-// #ifndef _WIN32
-// 	available = available && MultitrackVideoDeveloperModeEnabled();
-// #endif
+#ifndef _WIN32
+	available = available && MultitrackVideoDeveloperModeEnabled();
+#endif
 
 	if (IsCustomService())
 		available = available && MultitrackVideoDeveloperModeEnabled();
