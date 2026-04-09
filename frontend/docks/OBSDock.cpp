206a0b05b2f3ce978c1c9bab41bd7d056679137c
John Mickelonis <jmick@email.com>
2026-04-09 03:35:40 -0400

    Implemented TitleBarWidget, so we can avoid using native title bars. This allows the UI to be consistent across platforms.

--- frontend/docks/OBSDock.cpp
+++ frontend/docks/OBSDock.cpp
@@ -4,14 +4,218 @@
 
 #include <QCheckBox>
 #include <QMessageBox>
+#include <QStylePainter>
 
 #include "moc_OBSDock.cpp"
+
+TitleBarWidget::TitleBarWidget(OBSDock *dock) : QWidget(dock)
+{
+	// Don't cover up the dock widget
+	setAttribute(Qt::WA_NoSystemBackground);
+	setAttribute(Qt::WA_TransparentForMouseEvents);
+
+	// Get the dock's buttons (hopefully these will never be null!)
+	floatButton = dock->findChild<QAbstractButton *>("qt_dockwidget_floatbutton");
+	closeButton = dock->findChild<QAbstractButton *>("qt_dockwidget_closebutton");
+
+	TitleBarLayout *layout = new TitleBarLayout(this);
+	layout->setWidget(TitleBarLayout::FloatButton, floatButton);
+	layout->setWidget(TitleBarLayout::CloseButton, closeButton);
+
+	connect(dock, &QDockWidget::featuresChanged, this, &TitleBarWidget::onFeaturesChanged);
+	connect(dock, &QDockWidget::topLevelChanged, this, &TitleBarWidget::onTopLevelChanged);
+}
+
+bool TitleBarWidget::event(QEvent *event)
+{
+	switch (event->type()) {
+
+	case QEvent::ApplicationLayoutDirectionChange:
+	case QEvent::LayoutDirectionChange:
+	case QEvent::ParentChange:
+	case QEvent::StyleChange:
+		layout()->invalidate();
+		break;
+
+	default:
+		break;
+	}
+
+	return QWidget::event(event);
+}
+
+OBSDock *TitleBarWidget::getDock() const
+{
+	return qobject_cast<OBSDock *>(parentWidget());
+}
+
+void TitleBarWidget::onFeaturesChanged(QDockWidget::DockWidgetFeatures)
+{
+	layout()->invalidate();
+}
+
+void TitleBarWidget::onTopLevelChanged(bool)
+{
+	layout()->invalidate();
+
+	// Stop from showing hover after setting floatable
+	floatButton->setAttribute(Qt::WA_UnderMouse, false);
+
+	// Activate the window when [un]floating
+	QWindow *window = this->window()->windowHandle();
+	if (window)
+		window->requestActivate();
+}
+
+TitleBarLayout::TitleBarLayout(QWidget *parent) : QLayout(parent), items(2, nullptr) {}
+
+TitleBarLayout::~TitleBarLayout()
+{
+	qDeleteAll(items);
+}
+
+QWidget *TitleBarLayout::getWidget(Role role) const
+{
+	QLayoutItem *item = items.at(role);
+	return item ? item->widget() : nullptr;
+}
+
+void TitleBarLayout::setWidget(Role role, QWidget *widget)
+{
+	QWidget *old = getWidget(role);
+
+	if (old) {
+		old->hide();
+		removeWidget(old);
+	}
+
+	if (widget) {
+		/* Do not add the button as a child,
+		 * because it's already a child of the dock itself
+		 */
+		//addChildWidget(widget);
+
+		items[role] = new QWidgetItemV2(widget);
+		widget->show();
+	} else {
+		items[role] = nullptr;
+	}
+
+	invalidate();
+}
+
+QLayoutItem *TitleBarLayout::itemAt(int index) const
+{
+	int count = 0;
+	for (QLayoutItem *item : items) {
+		if (!item)
+			continue;
+		if (index == count++)
+			return item;
+	}
+	return nullptr;
+}
+
+QLayoutItem *TitleBarLayout::takeAt(int index)
+{
+	int count = 0;
+	for (int i = 0; i < items.count(); i++) {
+		QLayoutItem *item = items.at(i);
+		if (!item)
+			continue;
+		if (index == count++) {
+			items[i] = nullptr;
+			invalidate();
+			return item;
+		}
+	}
+	return nullptr;
+}
+
+int TitleBarLayout::count() const
+{
+	int count = 0;
+	for (QLayoutItem *item : items)
+		if (item)
+			count++;
+	return count;
+}
+
+QSize TitleBarLayout::sizeHint() const
+{
+	OBSDock *dock = getDock();
+
+	bool floatable = dock->hasFeature(QDockWidget::DockWidgetFloatable);
+	bool closable = dock->hasFeature(QDockWidget::DockWidgetClosable);
+
+	QWidget *floatButton = items[FloatButton]->widget();
+	QWidget *closeButton = items[CloseButton]->widget();
+
+	/* Since the default updateButtons implementation can't be overridden,
+	 * we can set button visibility here.
+	 */
+	floatButton->setVisible(floatable);
+	closeButton->setVisible(closable);
+
+	QSize floatSize = floatable ? floatButton->sizeHint() : QSize(0, 0);
+	QSize closeSize = closable ? closeButton->sizeHint() : QSize(0, 0);
+
+	bool vertical = dock->hasFeature(QDockWidget::DockWidgetVerticalTitleBar);
+	int buttonHeight, w;
+
+	if (vertical) {
+		buttonHeight = qMax(floatSize.width(), closeSize.width());
+		w = floatSize.height() + closeSize.height();
+	} else {
+		buttonHeight = qMax(floatSize.height(), closeSize.height());
+		w = floatSize.width() + closeSize.width();
+	}
+
+	int margin = dock->style()->pixelMetric(QStyle::PM_DockWidgetTitleMargin, nullptr, dock);
+	int h = qMax(dock->fontMetrics().height(), buttonHeight) + margin * 2;
+	w += h + margin * 3;
+	return vertical ? QSize(h, w) : QSize(w, h);
+}
+
+void TitleBarLayout::setGeometry(const QRect &)
+{
+	OBSDock *dock = getDock();
+
+	QStyleOptionDockWidget opt;
+	dock->initStyleOption(&opt);
+
+	QStyle *style = dock->style();
+	QRect rect = style->subElementRect(QStyle::SE_DockWidgetCloseButton, &opt, dock);
+	if (!rect.isNull())
+		items[CloseButton]->setGeometry(rect);
+	rect = style->subElementRect(QStyle::SE_DockWidgetFloatButton, &opt, dock);
+	if (!rect.isNull())
+		items[FloatButton]->setGeometry(rect);
+}
+
+QSize TitleBarLayout::minimumSize() const
+{
+	return sizeHint();
+}
+
+OBSDock *TitleBarLayout::getDock() const
+{
+	return qobject_cast<OBSDock *>(parentWidget()->parentWidget());
+}
 
 OBSDock::OBSDock(const QString &title, QWidget *parent) : QDockWidget(title, parent)
 {
 	mouseState = NotPressed;
 	settingFlags = false;
 	installEventFilter(this);
+
+	TitleBarWidget *titleBar = new TitleBarWidget(this);
+	setTitleBarWidget(titleBar);
+}
+
+bool OBSDock::hasFeature(QDockWidget::DockWidgetFeature feature)
+{
+	return features() & feature;
 }
 
 void OBSDock::setVisible(bool visible)
@@ -122,6 +326,26 @@ bool OBSDock::eventFilter(QObject *o, QEvent *e)
 	}
 
 	return QDockWidget::eventFilter(o, e);
+}
+
+void OBSDock::paintEvent(QPaintEvent *)
+{
+	QStylePainter painter(this);
+
+	if (isFloating()) {
+		QStyleOptionFrame opt;
+		opt.initFrom(this);
+		painter.drawPrimitive(QStyle::PE_FrameDockWidget, opt);
+	}
+
+	QStyleOptionDockWidget opt;
+	initStyleOption(&opt);
+	const QFont font = this->font();
+	if (font == QApplication::font("QDockWidget")) {
+		opt.fontMetrics = QFontMetrics(font);
+		painter.setFont(font);
+	}
+	painter.drawControl(QStyle::CE_DockWidgetTitle, opt);
 }
 
 void OBSDock::setTranslucent(bool value)
--- frontend/docks/OBSDock.hpp
+++ frontend/docks/OBSDock.hpp
@@ -1,10 +1,64 @@
 #pragma once
 
 #include <QDockWidget>
+#include <QAbstractButton>
+#include <QLayout>
 
 class QCloseEvent;
 class QShowEvent;
 class QString;
+class OBSDock;
+
+/* Handles display and positioning of the title bar buttons,
+ * so we can avoid using native title bars
+ * (allowing the UI to be consistent across platforms).
+ */
+class TitleBarWidget : public QWidget {
+	Q_OBJECT
+
+public:
+	TitleBarWidget(OBSDock *dock);
+
+protected:
+	virtual bool event(QEvent *event) override;
+
+private:
+	OBSDock *getDock() const;
+
+	QAbstractButton *closeButton;
+	QAbstractButton *floatButton;
+
+private slots:
+	void onFeaturesChanged(QDockWidget::DockWidgetFeatures features);
+	void onTopLevelChanged(bool value);
+};
+
+class TitleBarLayout : public QLayout {
+
+public:
+	TitleBarLayout(QWidget *parent);
+	~TitleBarLayout();
+
+	enum Role { FloatButton, CloseButton };
+	QWidget *getWidget(Role role) const;
+	void setWidget(Role role, QWidget *widget);
+
+	QLayoutItem *itemAt(int index) const override;
+	QLayoutItem *takeAt(int index) override;
+	void addItem(QLayoutItem *) override {}
+	int count() const override;
+
+	QSize sizeHint() const override;
+
+protected:
+	void setGeometry(const QRect &rect) override;
+	QSize minimumSize() const override;
+
+private:
+	QVector<QLayoutItem *> items;
+
+	OBSDock *getDock() const;
+};
 
 class OBSDock : public QDockWidget {
 	Q_OBJECT
@@ -13,17 +67,22 @@ public:
 	OBSDock(const QString &title, QWidget *parent = nullptr);
 	inline OBSDock(QWidget *parent = nullptr) : OBSDock("", parent) {}
 
+	bool hasFeature(QDockWidget::DockWidgetFeature feature);
 	virtual void setVisible(bool visible) override;
 	virtual void closeEvent(QCloseEvent *event);
 	virtual void showEvent(QShowEvent *event);
 
 protected:
+	friend class TitleBarLayout;
+	friend class TitleBarWidget;
+
 	virtual bool eventFilter(QObject *o, QEvent *e) override;
+	virtual void paintEvent(QPaintEvent *event) override;
 
 private:
-	void setTranslucent(bool value);
-
 	enum MouseState { NotPressed, Pressed, Dragging };
 	MouseState mouseState;
 	bool settingFlags;
+
+	void setTranslucent(bool value);
 };
